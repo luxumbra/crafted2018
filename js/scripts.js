8cb0152f4030ab41414fcbe7860f0c05e35b6c32
Dave Sayer <hello@crafted.im>
2018-10-04 19:17:02 +0100

    layout improvement and logo added

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ var options = {
     strings: ["Web devl", "Web development", "Web design", "Ecommerce ^1000", "Hand crafted websites made in the Isle of Man"],
     typeSpeed: 100,
     backDelay: 200,
-    fadeOut: true,
+    fadeOut: false,
     loop: false,
     loopCount: Infinity
 }
