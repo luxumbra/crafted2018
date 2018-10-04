4586d6c35ddd50266505606f9c810ec216cf7cab
Dave Sayer <hello@crafted.im>
2018-10-04 17:37:07 +0100

    Updates to styling

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ var options = {
     strings: ["Web devl", "Web development", "Web design", "Ecommerce ^1000", "Hand crafted websites made in the Isle of Man"],
     typeSpeed: 100,
     backDelay: 200,
+    fadeOut: true,
     loop: false,
     loopCount: Infinity
 }
