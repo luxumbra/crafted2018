b30ef37198d53160c39e0b15a5733c25ba137d6b
Dave Sayer <hello@crafted.im>
2018-10-06 21:31:21 +0100

    about section and some style tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 
 /* typed effect */
 var options = {
-    strings: ["Web devl", "Web development", "Web design", "Ecommerce ^1000", "Hand crafted websites made in the Isle of Man"],
+    strings: ["Web development", "Web design", "Ecommerce ^1000", "Hand crafted websites made in the Isle of Man"],
     typeSpeed: 100,
     backDelay: 200,
     fadeOut: false,
